e67841e47bd4c2369dfe220464d3dfafa46762d1
alasteralfio <alasteralfio@gmail.com>
2023-06-27 13:19:57 +0800

    Create 27june.py

---
+++ June 2023/27june.py
@@ -0,0 +1,12 @@
+# CodeWars
+# 27/6/2023
+
+# https://www.codewars.com/kata/568d0dd208ee69389d000016/train/python
+def rental_car_cost(d):
+    if d >= 7:
+        return (d * 40) - 50
+    elif d >= 3:
+        return (d * 40) - 20
+    else: return d * 40
+
+# wont do much today, will be learning more advanced python + basic javascript
